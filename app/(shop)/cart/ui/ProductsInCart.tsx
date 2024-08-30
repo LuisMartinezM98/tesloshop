bf3e75b9f4b91903d49b6d33d8c320cb7d2c5a41
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2024-08-30 13:09:47 -0600

    Carrito de compras logica terminada

--- app/(shop)/cart/ui/ProductsInCart.tsx
+++ app/(shop)/cart/ui/ProductsInCart.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { useEffect, useState } from "react";
+import { EventHandler, MouseEventHandler, useEffect, useState } from "react";
 import Image from "next/image";
 import { useCartStore } from "@/store";
 import { QuantitySelector } from "@/components";
 import Link from "next/link";
+import { CartProduct } from "@/interfaces";
 
 
 
@@ -12,17 +13,23 @@ export const ProductsInCart = () => {
     const updateProductQuantity = useCartStore(state => state.updateProductQuantity);
     const [loaded, setLoaded] = useState(false);
     const productsInCart = useCartStore(state => state.cart);
-    const [quantityUpdate, setQuantityUpdate] = useState(0);
+    const removeProduct = useCartStore(state => state.removeProduct);
 
     useEffect(() => {
         setLoaded(true)
     }, [])
 
-    console.log(quantityUpdate);
 
     if(!loaded) {
         return <p>Loading...</p>
     }
+
+    const handleRemoveItem = (product: CartProduct): MouseEventHandler<HTMLButtonElement> => {
+      return (e) => {
+        e.preventDefault();
+        removeProduct(product);
+      };
+    };
   return (
     <>
     {productsInCart.map( product => (
@@ -44,9 +51,12 @@ export const ProductsInCart = () => {
                     {product.size} - {product.title}
                     </Link>
                     <p>${product.price}</p>
-                    <QuantitySelector quantity={product.quantity} onQuantityChanged={setQuantityUpdate} stock={product.stock}/>
+                    <QuantitySelector quantity={product.quantity}
+                    onQuantityChanged={quantity => updateProductQuantity(product, quantity)}
+                    stock={product.stock}
+                    />
 
-                    <button className="underline mt-3">Remover</button>
+                    <button className="underline mt-3" onClick={handleRemoveItem(product)}>Remover</button>
                   </div>
               </div>
             ))}
--- store/cart/cart-store.ts
+++ store/cart/cart-store.ts
@@ -6,12 +6,16 @@ import { persist } from "zustand/middleware";
 interface State {
 
     cart: CartProduct[];
-
     getTotalItems: () => number;
-
+    getSummaryInformation: () => {
+        subTotal: number;
+        tax: number;
+        total: number;
+        itemsInCart: number;
+    };
     addProductToCart: (product: CartProduct) => void;
     updateProductQuantity: (product: CartProduct, quantity: number) => void;
-    // removeProduct
+    removeProduct: (product: CartProduct) => void;
 }
 
 export const useCartStore = create<State>()(
@@ -24,7 +28,18 @@ export const useCartStore = create<State>()(
                 const { cart } = get();
                 return cart.reduce((total, item) => total + item.quantity, 0)
             },
+            getSummaryInformation: () => {
+                const { cart } = get();
+                const subTotal = cart.reduce((subtotal, item) => subtotal + (item.price * item.quantity), 0);
+                const tax = subTotal * 0.15;
+                const total = subTotal + tax;
 
+                const itemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
+                return {
+                    subTotal, tax, total, itemsInCart
+                }
+            },
             addProductToCart: (product: CartProduct) => {
                 const { cart } = get();
                 //1. Revisar si el producto existe en el carrito con la talla seleccionada
@@ -46,9 +61,19 @@ export const useCartStore = create<State>()(
                 set({ cart: updatedCartProducts });
             },
             updateProductQuantity: (product: CartProduct, quantity: number) => {
-                const {cart} = get();
-                const productInCart = cart.find(item => item.id === product.id && item.size === product.size);
-                console.log(productInCart)
+                const { cart } = get();
+                const updatedCartProducts = cart.map(item => {
+                    if (item.id === product.id && item.size === product.size) {
+                        return { ...item, quantity: quantity }
+                    }
+                    return item
+                });
+                set({ cart: updatedCartProducts });
+            },
+            removeProduct: (product: CartProduct) => {
+                const { cart } = get();
+                const removeCartProduct = cart.filter(item => item.id !== product.id || item.size !== product.size );
+                set({ cart: removeCartProduct });
             }
         }),
         {
